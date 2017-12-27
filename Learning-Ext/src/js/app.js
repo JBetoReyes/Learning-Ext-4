22a1ae644ab0ff6cb9e2764af4dfa77e312bc143
JBetoReyes <beto.kellyano@gmail.com>
2017-12-26 19:40:55 -0600

    Type     Scope         Description 1.-Refactor Extindex.html working with dom elements

--- Learning-Ext/src/js/app.js
+++ Learning-Ext/src/js/app.js
@@ -4,8 +4,39 @@ Ext.require([
 ]);
 
 Ext.onReady(function() {
-  var myDiv1 = Ext.get('div1');
-  // myDiv1.setSize(350, 350, {duration: 750, easing:'bounceOut'});
-    setTimeout(function(){ Ext.Msg.alert("Alert","We're ready to go!"); }, 10000);
+  var myDiv1 = Ext.get('div1');  
+  myDiv1.createChild({
+    tag: 'div',
+    id: 'nestedDiv',
+    style: 'border: 1px dashed;' +
+           'padding: 5px;',
+    children: {
+      tag: 'div',
+      html: '... a nested div',
+      style: 'color: #EE0000;' + 
+             'border: 1px solid;'
+    }
+  });
+
+  myDiv1.insertFirst({
+    tag: 'div',
+    html: 'Child inserted as node 0 of myDiv1',
+    style: 'padding: 5px;'
+  });
+
+  myDiv1.createChild({
+    tag: 'div',
+    html: 'Child inserted as node 2 of myDiv1',
+    id: 'removeMeLater',
+    style: 'padding: 5px;'
+  }, myDiv1.dom.childNodes[3]);
+
+  setTimeout(function(){
+    myDiv1.load({
+      url: 'fragments/htmlFragment.html',
+      scripts: true
+    }, 10000);
+  })
+
 });
 
