d28e57dd4df65939816cd58d66e6dfbcbc4b3946
JBetoReyes <beto.kellyano@gmail.com>
2017-12-27 02:32:27 +0000

    Type      Scope Description 1.-Feature   app    Using more complex templates

--- Learning-Ext/src/js/app.js
+++ Learning-Ext/src/js/app.js
@@ -1,6 +1,26 @@
 Ext.onReady(function(){
-  var myTpl = Ext.create('Ext.Template', '<div class="myDiv">Hello {0}.</div>');
-  myTpl.append(document.body, ['Marjan']);
-  myTpl.append(document.body, ['Michael']);
-  myTpl.append(document.body, ['Sebastian']);
+  var myTpl = Ext.create('Ext.Template', [
+    '<div style="background-color: {color}; margin: 10px;>"',
+      '<b> Name :</b> {name}<br/>',
+      '<b> Age :</b> {age} <br/>',
+      '<b> DOB :</b> {dob} <br/>',
+    '</div>'
+  ]);
+
+  myTpl.compile();
+
+  myTpl.append(document.body,{
+    color: "#E9E9FF",
+    name: 'John Smith',
+    age: 20,
+    dob: '10/20/89'
+  });
+
+  myTpl.append(document.body,{
+    color: "#FFE9E9",
+    name: 'Naomi White',
+    age: 25,
+    dob: '03/17/84'
+  });
+
 });
